27a521e56a6d033b10146825be30d39d82eddda3
gopikagopi2296 <164781306+gopikagopi2296@users.noreply.github.com>
2026-02-26 16:30:33 +0530

    Update settings.py

--- insurance/settings.py
+++ insurance/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k(!y22i-rb(h0+!@h1b$)%)c(n!3og7-^e=%==vcnz9g-z4bis'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
